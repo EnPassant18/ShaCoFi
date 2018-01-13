6967ae4b39b7294a5897a1ad413b7d39057f6522
Daniel <daniel_kostovetsky@brown.edu>
2018-01-12 21:52:22 -0500

    created first user interface

---
+++ Shacofi/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Shacofi
+//
+//  Created by Daniel Kostovetsky on 1/8/18.
+//  Copyright © 2018 Daniel Kostovetsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
