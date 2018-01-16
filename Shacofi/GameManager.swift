fec6d953d0041ad6275e3db93da87250af9403fd
Daniel <daniel_kostovetsky@brown.edu>
2018-01-16 10:38:00 -0500

    Completed data model

--- Shacofi/GameManager.swift
+++ Shacofi/GameManager.swift
@@ -30,13 +30,24 @@ enum Tile {
         switch self {
         case .blank:
             return #imageLiteral(resourceName: "Blank")
-        case .tile(let color, let fill, let shape, let selected):
+        case let .tile(color, fill, shape, selected):
             if selected {
                 return UIImage(named: "\(color)\(fill)\(shape)*")!
             } else {
                 return UIImage(named: "\(color)\(fill)\(shape)")!
             }
             
+        }
+    }
+    
+    // Returns a new Tile.tile with the Bool value negated
+    // Does nothing for a Tile.blank
+    func toggleSelected() -> Tile? {
+        switch self {
+        case let .tile(color, fill, shape, selected):
+            return Tile.tile(color, fill, shape, !selected)
+        default:
+            return nil
         }
     }
     
@@ -53,22 +64,49 @@ enum Tile {
     }
     
     static func isTrio(_ tile1: Tile, _ tile2: Tile, _ tile3: Tile) -> Bool {
-        return true //STUB
+        switch (tile1, tile2, tile3) {
+        case (.tile(let color1, let fill1, let shape1, true),
+              .tile(let color2, let fill2, let shape2, true),
+              .tile(let color3, let fill3, let shape3, true)):
+            let correctColors =
+                ((color1 == color2) && (color2 == color3)) ||
+                ((color1 != color2) && (color2 != color3))
+            let correctFills =
+                ((fill1 == fill2) && (fill2 == fill3)) ||
+                ((fill1 != fill2) && (fill2 != fill3))
+            let correctShapes =
+                ((shape1 == shape2) && (shape2 == shape3)) ||
+                ((shape1 != shape2) && (shape2 != shape3))
+            return correctColors && correctFills && correctShapes
+        default:
+            return false
+        }
     }
 }
 
 
 class Game {
     
-    var board: [[Tile]]
-    
-    init() {
-        board = [
+    var outlets: [[UIButton]]
+    var selected: [(Int, Int)] = []
+    var board: [[Tile]] = [
         [Tile.blank, Tile.blank, Tile.blank],
         [Tile.blank, Tile.blank, Tile.blank],
         [Tile.blank, Tile.blank, Tile.blank],
         [Tile.blank, Tile.blank, Tile.blank],
         [Tile.blank, Tile.blank, Tile.blank]]
+    
+    init(outlets: [[UIButton]]) {
+        self.outlets = outlets
+    }
+    
+    func updateDisplay() {
+        for row in [1, 2, 3, 4, 5] {
+            for col in [1, 2, 3] {
+                outlets[row][col].setImage(board[row][col].getImage(),
+                                           for: .normal)
+            }
+        }
     }
     
     // Spawns three random tiles on the lowest blank row
@@ -80,7 +118,7 @@ class Game {
             return Tile.tile(color, fill, shape, false)
         }
         var insertRow: Int!
-        determineInsertRow: for row in [1, 2, 3, 4, 5] {
+        determineInsertRow: for row in [0, 1, 2, 3, 4] {
             if board[row][0] == Tile.blank {
                 insertRow = row
                 break determineInsertRow
@@ -88,12 +126,119 @@ class Game {
         }
         if let validRow = insertRow {
             board[validRow] = [spawnSingle(), spawnSingle(), spawnSingle()]
+            updateDisplay()
         } else {
             print("you lose") // STUB
         }
     }
     
-    func select(row: Int, col: Int) {
-        
+    /* Called when a user presses on a tile. If the tile is blank,
+    does nothing. If the tile is unselected, highlights it and records
+    the selection. If the tile is already selected, unhighlights it
+    and records the deselection. Also, if three tiles are now selected,
+    calls the _onThreeSelected method. */
+    func toggle(row: Int, col: Int) {
+        switch board[row][col] {
+        case Tile.tile(let color, let fill, let shape, true):
+            board[row][col] = Tile.tile(color, fill, shape, false)
+            updateDisplay()
+            selected = Array(selected.drop(while: { $0 == (row, col) }))
+        case Tile.tile(let color, let fill, let shape, false):
+            board[row][col] = Tile.tile(color, fill, shape, true)
+            updateDisplay()
+            selected.append((row, col))
+            if selected.count == 3 {
+                _onThreeSelected()
+            }
+        default:
+            break
+        }
+    }
+    
+    func _onThreeSelected() {
+        /* After a trio is deleted, determines if the board's columns
+        are of equal length, and if not, moves tiles to equalize them. */
+        func rebalance() {
+            // Moves a tile from one board position to another
+            func move(fromRow: Int, fromCol: Int, toRow: Int, toCol: Int) {
+                board[toRow][toCol] = board[fromRow][fromCol]
+                board[fromRow][fromCol] = Tile.blank
+            }
+            var col1Length = 0
+            var col2Length = 0
+            var col3Length = 0
+            for row in board {
+                if !(row[0] == Tile.blank) { col1Length += 1 }
+                if !(row[1] == Tile.blank) { col2Length += 1 }
+                if !(row[2] == Tile.blank) { col3Length += 1 }
+            }
+            if (col1Length != col2Length) && (col2Length != col3Length) {
+                switch max(col1Length, col2Length, col3Length) {
+                case col1Length:
+                    if col2Length > col3Length {
+                        move(fromRow: col1Length - 1, fromCol: 0,
+                             toRow: col3Length, toCol: 2)
+                    } else {
+                        move(fromRow: col1Length - 1, fromCol: 0,
+                             toRow: col2Length, toCol: 1)
+                    }
+                case col2Length:
+                    if col1Length > col3Length {
+                        move(fromRow: col2Length - 1, fromCol: 1,
+                             toRow: col3Length, toCol: 2)
+                    } else {
+                        move(fromRow: col2Length - 1, fromCol: 1,
+                             toRow: col1Length, toCol: 0)
+                    }
+                default:
+                    if col1Length > col2Length {
+                        move(fromRow: col3Length - 1, fromCol: 2,
+                             toRow: col2Length, toCol: 1)
+                    } else {
+                        move(fromRow: col3Length - 1, fromCol: 2,
+                             toRow: col1Length, toCol: 0)
+                    }
+                }
+            } else if (col1Length == col2Length) && (col2Length == col3Length) {
+                ()
+            } else {
+                switch min(col1Length, col2Length, col3Length) {
+                case col1Length:
+                    move(fromRow: col3Length - 1, fromCol: 2,
+                         toRow: col1Length, toCol: 0)
+                    move(fromRow: col2Length - 1, fromCol: 1,
+                         toRow: col1Length + 1, toCol: 0)
+                case col2Length:
+                    move(fromRow: col3Length - 1, fromCol: 2,
+                         toRow: col2Length, toCol: 1)
+                    move(fromRow: col1Length - 1, fromCol: 0,
+                         toRow: col2Length + 1, toCol: 1)
+                default:
+                    move(fromRow: col1Length - 1, fromCol: 0,
+                         toRow: col3Length, toCol: 2)
+                    move(fromRow: col2Length - 1, fromCol: 1,
+                         toRow: col3Length + 1, toCol: 2)
+                }
+            }
+        }
+        if Tile.isTrio(board[selected[0].0][selected[0].1],
+                       board[selected[1].0][selected[1].1],
+                       board[selected[2].0][selected[2].1]) {
+            board[selected[0].0][selected[0].1] = Tile.blank
+            board[selected[1].0][selected[1].1] = Tile.blank
+            board[selected[2].0][selected[2].1] = Tile.blank
+            selected = []
+            rebalance()
+            updateDisplay()
+        } else {
+            board[selected[0].0][selected[0].1] =
+                board[selected[0].0][selected[0].1].toggleSelected()!
+            board[selected[1].0][selected[1].1] =
+                board[selected[1].0][selected[1].1].toggleSelected()!
+            board[selected[2].0][selected[2].1] =
+                board[selected[2].0][selected[2].1].toggleSelected()!
+            selected = []
+            updateDisplay()
+        }
     }
 }
--- Shacofi/ViewController.swift
+++ Shacofi/ViewController.swift
@@ -10,63 +10,106 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: Initializer
+    
+    var game: Game!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        game = Game(outlets: [
+            [outletA1, outletB1, outletC1],
+            [outletA2, outletB2, outletC2],
+            [outletA3, outletB3, outletC3],
+            [outletA4, outletB4, outletC4],
+            [outletA5, outletB5, outletC5]])
     }
+    
+    //MARK: Actions
     
     @IBAction func Pause(_ sender: Any) {
     }
     
+    @IBAction func actionA5(_ sender: Any) {
+        game.toggle(row: 5, col: 1)
+    }
+    
+    @IBAction func actionB5(_ sender: Any) {
+        game.toggle(row: 5, col: 2)
+    }
+    
+    @IBAction func actionC5(_ sender: Any) {
+        game.toggle(row: 5, col: 3)
+    }
+    
+    @IBAction func actionA4(_ sender: Any) {
+        game.toggle(row: 4, col: 1)
+    }
+    
+    @IBAction func actionB4(_ sender: Any) {
+        game.toggle(row: 4, col: 2)
+    }
+    
+    @IBAction func actionC4(_ sender: Any) {
+        game.toggle(row: 4, col: 3)
+    }
+    
+    @IBAction func actionA3(_ sender: Any) {
+        game.toggle(row: 3, col: 1)
+    }
+    
+    @IBAction func actionB3(_ sender: Any) {
+        game.toggle(row: 3, col: 2)
+    }
+    
+    @IBAction func actionC3(_ sender: Any) {
+        game.toggle(row: 3, col: 3)
+    }
+    
+    @IBAction func actionA2(_ sender: Any) {
+        game.toggle(row: 2, col: 1)
+    }
+    
+    @IBAction func actionB2(_ sender: Any) {
+        game.toggle(row: 2, col: 2)
+    }
+    
+    @IBAction func actionC2(_ sender: Any) {
+        game.toggle(row: 2, col: 3)
+    }
+    
+    @IBAction func actionA1(_ sender: Any) {
+        game.toggle(row: 1, col: 1)
+    }
+    
+    @IBAction func actionB1(_ sender: Any) {
+        game.toggle(row: 1, col: 2)
+    }
+    
+    @IBAction func actionC1(_ sender: Any) {
+        game.toggle(row: 1, col: 3)
+    }
+    
+    
+    //MARK: Outlets
+    
     @IBOutlet weak var score: UILabel!
     @IBOutlet weak var best: UILabel!
     
-    
-    @IBAction func A5(_ sender: Any) {
-        Game.select(row: 5, col: 1)
-    }
-    @IBOutlet weak var A5: UIButton!
-    @IBAction func B5(_ sender: Any) {
-    }
-    @IBOutlet weak var B5: UIButton!
-    @IBAction func C5(_ sender: Any) {
-    }
-    @IBOutlet weak var C5: UIButton!
-    @IBAction func A4(_ sender: Any) {
-    }
-    @IBOutlet weak var A4: UIButton!
-    @IBAction func B4(_ sender: Any) {
-    }
-    @IBOutlet weak var B4: UIButton!
-    @IBAction func C4(_ sender: Any) {
-    }
-    @IBOutlet weak var C4: UIButton!
-    @IBAction func A3(_ sender: Any) {
-    }
-    @IBOutlet weak var A3: UIButton!
-    @IBAction func B3(_ sender: Any) {
-    }
-    @IBOutlet weak var B3: UIButton!
-    @IBAction func C3(_ sender: Any) {
-    }
-    @IBOutlet weak var C3: UIButton!
-    @IBAction func A2(_ sender: Any) {
-    }
-    @IBOutlet weak var A2: UIButton!
-    @IBAction func B2(_ sender: Any) {
-    }
-    @IBOutlet weak var B2: UIButton!
-    @IBAction func C2(_ sender: Any) {
-    }
-    @IBOutlet weak var C2: UIButton!
-    @IBAction func A1(_ sender: Any) {
-    }
-    @IBOutlet weak var A1: UIButton!
-    @IBAction func B1(_ sender: Any) {
-    }
-    @IBOutlet weak var B1: UIButton!
-    @IBAction func C1(_ sender: Any) {
-    }
-    @IBOutlet weak var C1: UIButton!
+    @IBOutlet weak var outletA5: UIButton!
+    @IBOutlet weak var outletB5: UIButton!
+    @IBOutlet weak var outletC5: UIButton!
+    @IBOutlet weak var outletA4: UIButton!
+    @IBOutlet weak var outletB4: UIButton!
+    @IBOutlet weak var outletC4: UIButton!
+    @IBOutlet weak var outletA3: UIButton!
+    @IBOutlet weak var outletB3: UIButton!
+    @IBOutlet weak var outletC3: UIButton!
+    @IBOutlet weak var outletA2: UIButton!
+    @IBOutlet weak var outletB2: UIButton!
+    @IBOutlet weak var outletC2: UIButton!
+    @IBOutlet weak var outletA1: UIButton!
+    @IBOutlet weak var outletB1: UIButton!
+    @IBOutlet weak var outletC1: UIButton!
     
 }
 
